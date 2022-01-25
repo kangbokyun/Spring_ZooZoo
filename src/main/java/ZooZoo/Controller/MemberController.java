eef5a9c2af9e9ab372f76d11e6c09dfc189ee171
kangbokyun <bk940204@naver.com>
2022-01-25 21:40:46 +0900

    최종 수정  2022_01_25

--- src/main/java/ZooZoo/Controller/MemberController.java
+++ src/main/java/ZooZoo/Controller/MemberController.java
@@ -1,18 +1,38 @@
 package ZooZoo.Controller;
 
+import ZooZoo.Domain.DTO.MemberDTO;
+import ZooZoo.Service.MemberService;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
+    @Autowired
+    MemberService memberService;
+
     // 시작 - 메인화면
     @GetMapping("/")
-    public String goToMain() {return "main";}
+    public String goToMain() {return "Main";}
     // 로그인화면으로
     @GetMapping("/Member/Login")
     public String goToLogin() {return "Member/Login";}
     // 회원가입으로
     @GetMapping("/Member/SignUp")
     public String goToSignUp() {return "Member/SignUp";}
+
+    // 회원가입
+    @PostMapping("/Member/SignUpController") @ResponseBody
+    public String SignUp(MemberDTO memberDTO) {
+        System.out.println(memberDTO.getMid());
+        boolean result = memberService.SignUp(memberDTO);
+        if(result) {
+            return "1";
+        } else {
+            return "2";
+        }
+    }
 }
--- src/main/java/ZooZoo/Service/MemberService.java
+++ src/main/java/ZooZoo/Service/MemberService.java
@@ -1,8 +1,27 @@
 package ZooZoo.Service;
 
+import ZooZoo.Domain.DTO.MemberDTO;
+import ZooZoo.Domain.Entity.MemberEntity;
+import ZooZoo.Domain.Entity.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
 
+    // 회원가입
+    public boolean SignUp(MemberDTO memberDTO) {
+        MemberEntity memberEntity = MemberEntity.builder()
+                .mid(memberDTO.getMid())
+                .mpw(memberDTO.getMpw())
+                .mname(memberDTO.getMname())
+                .memail(memberDTO.getMemail())
+                .mbirth(memberDTO.getMbirth())
+                .maddress(memberDTO.getMaddress())
+                .build();
+        memberRepository.save(memberEntity);
+        return true;
+    }
 }
