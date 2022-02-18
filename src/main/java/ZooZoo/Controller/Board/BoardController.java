6c7e21819692571f67edd5c225561e36727c355d
jyb <alex.ybjang@gmail.com>
2022-02-18 19:43:42 +0900

    공지 페이지 옵션 검색 완료

--- src/main/java/ZooZoo/Controller/Board/BoardController.java
+++ src/main/java/ZooZoo/Controller/Board/BoardController.java
@@ -398,19 +398,17 @@ public class BoardController {
         String sex = request.getParameter("sex"); // 성별
         String kind = request.getParameter("kind"); // 축종
         String city = request.getParameter("city"); // 시군구
-        String state = request.getParameter("state"); // 상태
+        String state = "total"; // 상태
         HttpSession session = request.getSession();
 
-        if (sex != null || kind != null || city != null || state != null) {
+        if (sex != null || kind != null || city != null) {
             session.setAttribute("sex", sex);
             session.setAttribute("kind", kind);
             session.setAttribute("city", city);
-            session.setAttribute("state", state);
         } else {
             sex = (String) session.getAttribute("sex");
             kind = (String) session.getAttribute("kind");
             city = (String) session.getAttribute("city");
-            state = (String) session.getAttribute("state");
         }
 
 
